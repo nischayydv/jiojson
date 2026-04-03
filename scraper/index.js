1bc61b799ec5b2b678a99cf6dbc63d3c0afa6a70
nischayydv <nischay419@gmail.com>
2026-04-03 09:42:14 +0530

    Update index.js

--- scraper/index.js
+++ scraper/index.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 const fs = require("fs");
 
-const STREAM_URL = "https://perelive.pages.dev/jiotv.m3u";
+const STREAM_URL = "https://pocket-tv-tamil-5afe35.gitlab.io/jiostar.m3u";
 const OUTPUT_FILE = "stream.json";
 
 async function fetchAndSaveJson() {
