7a9c77e24e7d84cdb19a03d2ae04ea1ee719ffe8
nischayydv <nischay419@gmail.com>
2026-04-03 09:40:46 +0530

    Update index.js

--- scraper/index.js
+++ scraper/index.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 const fs = require("fs");
 
-const STREAM_URL = "https://netx.streamstar18.workers.dev/zio1";
+const STREAM_URL = "https://perelive.pages.dev/jiotv.m3u";
 const OUTPUT_FILE = "stream.json";
 
 async function fetchAndSaveJson() {
