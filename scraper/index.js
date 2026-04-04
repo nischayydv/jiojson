e1eed243ce922c77c6b1b5b8666c39b658ef17e4
nischayydv <nischay419@gmail.com>
2026-04-04 17:04:37 +0530

    Update index.js

--- scraper/index.js
+++ scraper/index.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 const fs = require("fs");
 
-const STREAM_URL = "https://jiotvandsony.teachub.workers.dev/?t=teachub";
+const STREAM_URL = "https://servertvhub.site/api/playlist.php";
 const OUTPUT_FILE = "stream.json";
 
 async function fetchAndSaveJson() {
