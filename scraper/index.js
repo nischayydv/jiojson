b0e5b453f16158033ad72662710ccdfc45330a85
nischayydv <nischay419@gmail.com>
2026-04-04 16:27:16 +0530

    Update index.js

--- scraper/index.js
+++ scraper/index.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 const fs = require("fs");
 
-const STREAM_URL = "https://server.lrl45.workers.dev/channel/raw?=m3u";
+const STREAM_URL = "https://server.vodep39240327.workers.dev/channel/raw?=m3u";
 const OUTPUT_FILE = "stream.json";
 
 async function fetchAndSaveJson() {
