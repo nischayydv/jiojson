8db7bc5843e9262289f0e1a211548ff4b9515d69
nischayydv <nischay419@gmail.com>
2026-04-28 16:43:04 +0530

    Update index.js

--- scraper/index.js
+++ scraper/index.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 const fs = require("fs");
 
-const STREAM_URL = "https://allinonereborn.online/m3u/jtv145.m3u";
+const STREAM_URL = "https://servertvhub.site/superlive/playlist.php";
 const OUTPUT_FILE = "stream.json";
 
 async function fetchAndSaveJson() {
